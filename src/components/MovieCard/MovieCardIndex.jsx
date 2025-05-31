a976b7aaddb9825a5955bc087ce3e5d9867cf482
Cristopher Israel <cristopherisraeluaprj3@gmail.com>
2025-05-30 22:37:44 -0700

    Chore/CleanCode-&-Desing
    Cambios basicos de diseño y mejoras visuales para la presentacion de cartelera

--- src/components/MovieCard/MovieCardIndex.jsx
+++ src/components/MovieCard/MovieCardIndex.jsx
@@ -7,15 +7,17 @@ function MovieCard({ movie }) {
     <div className={styles.card}>
       <Link to={`/movie/${movie.id}`}>
         {movie.poster_path && (
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-          />
+          <p className={styles.center}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
+          </p>
         )}
       </Link>
       <h2>{movie.title}</h2>
       <p><strong>Fecha de estreno:</strong> {movie.release_date}</p>
-      <p>{movie.overview}</p>
+      <p className={styles.description}>{movie.overview}</p>
     </div>
   );
 }
